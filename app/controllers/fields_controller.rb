4455a94f0a0bd74c72579a905323968490a23639
akki0083 <fighters.nkt@gmail.com>
2020-02-10 15:32:50 +0900

    field_paramsを定義
    current_userと、params[:crop_id]に紐づけた。誰の何の作物が作られている圃場なのかの情報をのせるため。

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -4,4 +4,7 @@ class FieldsController < ApplicationController
     @crop = Crop.find(params[:crop_id])
     @field = Field.new
   end
+  def field_params
+    params.require(:field).permit(:name, :surface).merge(user_id: current_user.id, crop_id: params[:crop_id])
+  end
 end
