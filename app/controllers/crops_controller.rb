018f3b94548694db329c19fa997811330447585e
akki0083 <fighters.nkt@gmail.com>
2020-02-10 15:26:02 +0900

    作物のshowアクションを定義
    作物に紐づいた圃場のインスタンス変数を定義

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -4,4 +4,10 @@ class CropsController < ApplicationController
     @crops = Crop.all
     @fields = Field.all
   end
+
+  def show
+    @crop = Crop.find(params[:id])
+    @fields = Field.where(crop_id: params[:id])
+  end
+
 end
