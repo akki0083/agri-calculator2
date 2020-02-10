d3d6dbb53028759fc66749157f2dc716b98c5a5e
akki0083 <fighters.nkt@gmail.com>
2020-02-10 15:23:18 +0900

    圃場のルーティングを作成
    作物に紐ずけたいので、ネストさせた

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "crops#index"
   resources :crops, only: [:index, :show, :new, :create] do
+    resources :fields, only: [:new, :create]
   end
 end
