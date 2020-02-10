e3f2cbedc0bb3e289b88973f8d1295504cd853ba
akki0083 <fighters.nkt@gmail.com>
2020-02-10 15:33:55 +0900

    圃場のcreateアクションを定義

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -4,6 +4,18 @@ class FieldsController < ApplicationController
     @crop = Crop.find(params[:crop_id])
     @field = Field.new
   end
+
+  def create
+    @field = Field.new(field_params)
+    if @field.save
+      redirect_to root_path
+      flash[:alert] = '圃場を登録しました'
+    else
+      render :new
+      flash[:alert] = '圃場を登録できませんでした'
+    end
+  end
+
   def field_params
     params.require(:field).permit(:name, :surface).merge(user_id: current_user.id, crop_id: params[:crop_id])
   end
